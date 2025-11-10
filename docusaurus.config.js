b7379db401415094c372e6f6c02d7735be2f6527
Amlezia <alia.mezouar2@gmail.com>
2025-11-10 16:40:19 +0100

    feat: add new file docusaurus.config.js

---
+++ docusaurus.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+  presets: [
+    [
+      '@docusaurus/preset-classic',
+      {
+        docs: {
+          // Docs folder path relative to website dir.
+          path: '../docs',
+          // Sidebars file relative to website dir.
+          sidebarPath: require.resolve('./sidebars.json'),
+        },
+      },
+    ],
+  ],
+};
